16f401954c3642164bd7a4142b38a1488e81a6e3
Max Sky <max940213@live.com>
2018-06-07 23:55:40 +0800

    Merge from keka

---
+++ Translations/Web/lang_en.php
@@ -0,0 +1,69 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: English
+// Translated by aone (aone@keka.io)
+//
+//
+
+// General
+$content_download = "Download";
+$content_issues = "Issues";
+$content_help = "Help";
+$content_forum = "Forum";
+$content_changelog = "Changelog";
+$content_title = "the macOS file archiver";
+
+// Download
+$content_plataform = "Requires $version_replace or newer";
+$content_download_text = "Store more, share with privacy";
+$content_download_love = "Like it?";
+$content_download_legacy = "Legacy";
+
+// Like
+$content_donation_button = "Donate";
+$content_donation_title = 'If you like Keka just give it some <i class="fa fa-heart" aria-hidden="true"></i>,<br />get it from the App Store or send a PayPal tip!';
+$content_donation_mas = "If you buy Keka from the App Store, you will get the same as in this web<br />but it will be always updated through the App Store.";
+$content_donation_paypal = "If you don't like MAS or just want to play arround with Keka<br />but also like the project and want to give it some love, you can send a tip using PayPal.";
+
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Beta version of Keka";
+$content_beta_text = "You can test the newest features of Keka before they hit the stable status.<br />If you find some bug or just have anything to report, head to ";
+$content_beta_unavailable = "There are no betas available at this moment.";
+$content_beta_up = "Get the latest version";
+	
+
+// Legacy
+$content_plataform_legacy =  "For $version_replace";
+$content_legacy_title = "Legacy versions of Keka";
+$content_legacy_text = 'Over the years your Mac will become older and will not support<br />the newest Keka, but relax <i class="fa fa-coffee" aria-hidden="true"></i>, those will be with you.';
+
+// Info
+$content_info_title1 = "So simple, so strong";
+$content_info_title2 = "Privacy is very important";
+$content_info_title3 = "Still too big...";
+$content_info_text1 = "You do not even need to open Keka to compress a file, keep it in your dock and use it from there.<br />Just drag and drop your files and folders over it or over the Keka window to create a slimmer version of them.";
+$content_info_text2 = "Share safely just setting a password and creating highly encrypted files.<br />Using AES-256 encryption specification for your 7z files and<br />Zip 2.0 legacy encryption specification for your Zip files.";
+$content_info_text3 = "If the files are inmensely huge and they do not fit your mail or server, just split them into pieces.<br />Do not worry, they will rejoin to be your original file again :)";
+$content_info_compression = "Keka can create files using this formats:";
+$content_info_extraction = "And extract all this formats:";
+
+// Main content of the page
+$content_context_menu = "Context menu";
+
+// Changelog info
+$content_changelog_title = "Take a look at the Keka evolution";
+$content_changelog_entry_title = "Changes in version";
+$content_changelog_firstpublic = "First public release:";
+
+// 404
+$content_404 = "The page can’t be found";
+$content_404_start = "You can go to the <a href=\"https://www.keka.io\">start page</a>,";
+$content_404_more = "or maybe you want this?";
+
+// Bottom info
+$content_bottom_copying = 'All rights reserved.';
+
+?>
