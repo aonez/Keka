2c5980cedd7697b826a0927b6808f82ab195fe5d
Max Sky <max940213@live.com>
2018-06-07 23:55:40 +0800

    Merge from keka

--- Translations/Web/lang_en.php
+++ Translations/Web/lang_en.php
@@ -3,13 +3,7 @@
 //
 //
 // Language: English
-$content_language_locale = "en";
 // Translated by aone (aone@keka.io)
-//
-//
-// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
-$show_content_bottom_translator = false;
-$content_bottom_translator = 'Translated by <a href="https://yoursite.or.email.or.whatever">Yourself</a>.';
 //
 //
 
@@ -22,67 +16,51 @@ $content_changelog = "Changelog";
 $content_title = "the macOS file archiver";
 
 // Download
-$content_platform = "Requires $version_replace or newer";
+$content_plataform = "Requires $version_replace or newer";
 $content_download_text = "Store more, share with privacy";
 $content_download_love = "Like it?";
 $content_download_legacy = "Legacy";
-$content_download_helper = "Helper";
 
 // Like
 $content_donation_button = "Donate";
-$content_donation_title = 'If you like Keka just give it some <i class="fa fa-heart" aria-hidden="true"></i>, <br />get it from the App Store or send a PayPal tip!';
-$content_donation_mas = "If you buy Keka from the App Store you will be supporting development,<br />the app is the same as the version from this website only updated through the App Store.";
-$content_donation_paypal = "If you don't like MAS or just want to play around with Keka<br />but also like the project and want to give it some love, you can send a tip using PayPal.";
+$content_donation_title = 'If you like Keka just give it some <i class="fa fa-heart" aria-hidden="true"></i>,<br />get it from the App Store or send a PayPal tip!';
+$content_donation_mas = "If you buy Keka from the App Store, you will get the same as in this web<br />but it will be always updated through the App Store.";
+$content_donation_paypal = "If you don't like MAS or just want to play arround with Keka<br />but also like the project and want to give it some love, you can send a tip using PayPal.";
 
 // Beta
-$content_platform_beta = "Beta";
+$content_plataform_beta = "Beta";
 $content_beta_title = "Beta version of Keka";
-$content_beta_text = "You can test the newest features of Keka before they are released.<br />If you find a bug or have something to report, head over to ";
-$content_beta_unavailable = "There is no beta available at the moment.";
+$content_beta_text = "You can test the newest features of Keka before they hit the stable status.<br />If you find some bug or just have anything to report, head to ";
+$content_beta_unavailable = "There are no betas available at this moment.";
 $content_beta_up = "Get the latest version";
+	
 
 // Legacy
-$content_platform_legacy =  "For $version_replace";
+$content_plataform_legacy =  "For $version_replace";
 $content_legacy_title = "Legacy versions of Keka";
-$content_legacy_text = 'Over the years your Mac will become older and will not support<br />the newest Keka, but relax <i class="fa fa-coffee" aria-hidden="true"></i>, the old versions will be here.';
+$content_legacy_text = 'Over the years your Mac will become older and will not support<br />the newest Keka, but relax <i class="fa fa-coffee" aria-hidden="true"></i>, those will be with you.';
 
 // Info
 $content_info_title1 = "So simple, so strong";
 $content_info_title2 = "Privacy is very important";
 $content_info_title3 = "Still too big...";
-$content_info_text1 = "You do not even need to open Keka to compress a file, keep it in your dock and use it from there.<br />Just drag and drop your files and folders to the Dock icon or the Keka window to create a slimmer version of them.";
+$content_info_text1 = "You do not even need to open Keka to compress a file, keep it in your dock and use it from there.<br />Just drag and drop your files and folders over it or over the Keka window to create a slimmer version of them.";
 $content_info_text2 = "Share safely just setting a password and creating highly encrypted files.<br />Using AES-256 encryption specification for your 7z files and<br />Zip 2.0 legacy encryption specification for your Zip files.";
-$content_info_text3 = "If the files are really huge and they do not fit your mail or server, just split them into pieces.<br />Don't worry, they will still expand to produce your original file :)";
-$content_info_compression = "Keka can create files in these formats:";
-$content_info_extraction = "And extract all of these formats:";
-
-// Default app
-$content_defaultapp_title = "Set Keka as the default extraction application";
-$content_defaultapp_text = 'Keka <a href="https://github.com/aonez/Keka/wiki/Default-application">needs a helper <i class="far fa-question-circle"></i></a> to set itself as the default application.<br />Simply download and extract it, move it to the desktop and then open it.';
-
-// Terms of use
-$content_termsofuse_title = 'Terms of Use';
-$content_termsofuse_disclaimer = 'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
-$content_termsofuse_inapp_title = 'In-App Purchases';
-$content_termsofuse_inapp = 'The App Store version of Keka offers In-App purchases as a way to tip and help the development of the project, similar to <a href="https://www.keka.io/#lovekeka">PayPal/Sponsors in the website</a>. Those purchases are optional and do not grant any additional features.';
-
-// Privacy Policy
-$content_privacypolicy_title = 'Privacy Policy';
-$content_privacypolicy_content = 'Keka does not send or store any kind of data from your computer.';
-$content_privacypolicy_MAS = 'The MAS version does not have any network capabilities.';
-$content_privacypolicy_WEB = 'The WEB version uses Sparkle for the update process (that you can disable), so that component can connect to the internet. It is used solely to check for a new version and download it. This component never stores or shares any kind of information, not even the <a href="https://sparkle-project.org/documentation/system-profiling/">anonymous system profile data</a> that was enabled in some Keka versions prior to 1.0.0 but never used.';
+$content_info_text3 = "If the files are inmensely huge and they do not fit your mail or server, just split them into pieces.<br />Do not worry, they will rejoin to be your original file again :)";
+$content_info_compression = "Keka can create files using this formats:";
+$content_info_extraction = "And extract all this formats:";
 
 // Main content of the page
 $content_context_menu = "Context menu";
 
 // Changelog info
-$content_changelog_title = "Take a look at the evolution of Keka";
+$content_changelog_title = "Take a look at the Keka evolution";
 $content_changelog_entry_title = "Changes in version";
 $content_changelog_firstpublic = "First public release:";
 
 // 404
 $content_404 = "The page can’t be found";
-$content_404_start = "You can go to the <a href=\"https://www.keka.io\">start page</a>";
+$content_404_start = "You can go to the <a href=\"https://www.keka.io\">start page</a>,";
 $content_404_more = "or maybe you want this?";
 
 // Bottom info
--- Translations/Web/lang_es.php
+++ Translations/Web/lang_es.php
@@ -3,13 +3,7 @@
 //
 //
 // Language: Spanish
-$content_language_locale = "es";
 // Translated by aone (aone@keka.io)
-//
-//
-// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
-$show_content_bottom_translator = false;
-$content_bottom_translator = 'Translated by <a href="https://yoursite.or.email.or.whatever">Yourself</a>.';
 //
 //
 
@@ -22,49 +16,38 @@ $content_changelog = "Evolución";
 $content_title = "el compresor de macOS";
 
 // Download
-$content_platform = "Requiere $version_replace o superior";
+$content_plataform = "Requiere $version_replace o superior";
 $content_download_text = "Almacena más, comparte con privacidad";
 $content_download_love = "Te gusta?";
 $content_download_legacy = "Antiguas";
-$content_download_helper = "Ayudante";
 
 // Like
 $content_donation_button = "Donar";
-$content_donation_title = 'Si te gusta Keka dale un poco de <i class="fa fa-heart" aria-hidden="true"></i>, <br />cómpralo en la App Store o envía un donativo vía PayPal!';
+$content_donation_title = 'Si te gusta Keka dale un poco de <i class="fa fa-heart" aria-hidden="true"></i>,<br />cómpralo en la App Store o envía un donativo via PayPal!';
 $content_donation_mas = "Si compras Keka a través de la App Store, tendrás la mimsa versión que en la web<br />pero siempre se actualizará a través de la App Store.";
-$content_donation_paypal = "Si no te gusta la Apple Store o simplemente quieres jugar con las entrañas de Keka<br />pero además te gusta el proyecto y quieres darle tu apoyo, puedes enviar una donación utilizando PayPal.";
+$content_donation_paypal = "Si no te gusta la Apple Store o te símplemente quieres jugar con las entrañas de Keka<br />pero además te gusta el proyecto y quieres darle tu apoyo, puedes enviar una donación utilizando PayPal.";
 
 // Beta
-$content_platform_beta = "Beta";
+$content_plataform_beta = "Beta";
 $content_beta_title = "Versión beta de Keka";
 $content_beta_text = "Puedes probar las novedades de Keka antes de que lleguen a la versión estable.<br />Si encuentras algún problema o tienes algo que comentar, dirígete a ";
 $content_beta_unavailable = "No hay versiones beta disponibles.";
 $content_beta_up = "Descárgate la última versión";
 
 // Legacy
-$content_platform_legacy =  "Para $version_replace";
+$content_plataform_legacy =  "Para $version_replace";
 $content_legacy_title = "Versiones antiguas de Keka";
-$content_legacy_text = 'Con el paso de los años, tu Mac se va haciendo mayor y puede que haya dejado de soportar <br />la última versión de Keka, pero tranquilo <i class="fa fa-coffee" aria-hidden="true"></i>, éstas siempre estarán contigo.';
+$content_legacy_text = 'Con el paso de los años tu Mac se va haciendo mayor y puede que haya dejado de soportar <br />la última versión de Keka, pero tranquilo <i class="fa fa-coffee" aria-hidden="true"></i>, éstas siempre estarán contigo.';
 
 // Info
 $content_info_title1 = "Tan simple, tan potente";
 $content_info_title2 = "La privacidad es muy importante";
 $content_info_title3 = "¿Sigue siendo demasiado grande?";
 $content_info_text1 = "Ni siquiera necesitas abrir Keka para comprimir un archivo, manténlo en tu Dock y utilízalo desde ahí.<br />Simplemente arrastra y suelta los archivos y carpetas encima del icono o en la ventana de Keka para crear una versión menos pesada de ellos.";
-$content_info_text2 = "Con tan solo una contraseña podrás crear ficheros altamente encriptados que podrás compartir con toda seguridad.<br />Keka utiliza la especificación AES-256 para la encriptación de tus ficheros 7z<br />y Zip 2.0 legacy para tus ficheros Zip.";
-$content_info_text3 = "Si tus archivos son inmensamente enormes y no caben en tu correo o servidor, simplemente divídelos en trozos.<br />No te preocupes, volverán a juntarse de nuevo para convertirse en tu fichero original :)";
+$content_info_text2 = "Con tan solo una contraseña podrás crear ficheros áltamente encriptados que podrás compartir con toda seguridad.<br />Keka utiliza la especificación AES-256 para la encriptación de tus ficheros 7z<br />y Zip 2.0 legacy para tus ficheros Zip.";
+$content_info_text3 = "Si tus archivos son inmensamente enormes y no caben en tu correo o servidor, símplemente divídelos en trozos.<br />No te preocupes, volverán a juntarse de nuevo para a convertirse en tu fichero original :)";
 $content_info_compression = "Con Keka puedes crear archivos utilizando estos formatos:";
 $content_info_extraction = "Y extraer todos estos formatos:";
-
-// Default app
-$content_defaultapp_title = "Establecer Keka como el descompresor predeterminado";
-$content_defaultapp_text = 'Keka <a href="https://github.com/aonez/Keka/wiki/Default-application">necesita un ayudante <i class="far fa-question-circle"></i></a> para establecerse como la aplicación por defecto.<br />Simplemente descarga, extráe, mueve al escritorio y ejecuta el ayudante.';
-
-// Privacy Policy
-$content_privacypolicy_title = 'Política de privacidad';
-
-// Terms of use
-$content_termsofuse_title = 'Términos de uso';
 
 // Main content of the page
 $content_context_menu = "Menú contextual";
@@ -76,7 +59,7 @@ $content_changelog_firstpublic = "Primera versión pública:";
 
 // 404
 $content_404 = "No se ha encontrado lo que buscabas";
-$content_404_start = "Puedes ir a la <a href=\"https://www.keka.io/\">página inicial</a>";
+$content_404_start = "Puedes ir a la <a href=\"https://www.keka.io/\">página incial</a>,";
 $content_404_more = "¿o quizás buscabas esto?";
 
 // Bottom info
