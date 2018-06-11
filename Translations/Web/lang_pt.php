99ff9df5e78a2921e92911ef79baa74f8095ea08
Victor <contact@brlingo.com>
2018-06-10 21:01:47 -0400

    Site translation
    pt-BR translation of website.
    
    Signed-off-by: Victor <contact@brlingo.com>

---
+++ Translations/Web/lang_pt.php
@@ -0,0 +1,69 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: Portuguese
+// Translated by BR Lingo (contact@brlingo.com)
+//
+//
+
+// General
+$content_download = "Baixar";
+$content_issues = "Problemas";
+$content_help = "Ajuda";
+$content_forum = "Fórum";
+$content_changelog = "Histórico";
+$content_title = "O compressor de arquivos para macOS";
+
+// Download
+$content_plataform = "Requer $version_replace ou posterior";
+$content_download_text = "Armazene mais, compartilhe com privacidade";
+$content_download_love = "Curtindo?";
+$content_download_legacy = "Legado";
+
+// Like
+$content_donation_button = "Doar";
+$content_donation_title = 'Se você curte o Keka, demonstre seu <i class="fa fa-heart" aria-hidden="true"></i>;<br />obtenha-o na App Store ou faça uma doação pelo PayPal!';
+$content_donation_mas = "Ao comprar o Keka na App Store, você apoia o seu desenvolvimento.<br />O app é igual ao oferecido no site, só que as atualizações são feitas pela App Store.";
+$content_donation_paypal = "Se você não gosta da App Store ou quer apenas explorar o Keka<br />, mas ao mesmo tempo, curte o projeto e deseja demonstrar seu apoio, use o PayPal para fazer uma doação.";
+
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Versão beta do Keka";
+$content_beta_text = "Teste os recursos mais recentes do Keka antes do lançamento oficial.<br />Caso encontre erros ou tenha algo a comunicar, clique em ";
+$content_beta_unavailable = "Não há versões beta disponíveis no momento.";
+$content_beta_up = "Obtenha a versão mais recente";
+	
+
+// Legacy
+$content_plataform_legacy =  "Para $version_replace";
+$content_legacy_title = "Versões legadas do Keka";
+$content_legacy_text = 'Com o passar dos anos, seu Mac fica mais velho e pode não ser compatível<br />com a versão mais recente do Keka, mas tudo bem <i class="fa fa-coffee" aria-hidden="true"></i>, as versões mais antigas estarão por aqui.';
+
+// Info
+$content_info_title1 = "Tão simples, tão poderoso";
+$content_info_title2 = "Privacidade é muito importante";
+$content_info_title3 = "Ainda é grande demais?";
+$content_info_text1 = "Você não precisa nem abrir o Keka para comprimir arquivos, basta tê-lo no Dock.<br />Arraste e solte arquivos e pastas no ícone ou na janela do Keka para ter versões mais leves.";
+$content_info_text2 = "Com apenas uma senha, você cria arquivos comprimidos fortemente criptografados para compartilhar com toda segurança.<br />O Keka usa criptografia AES-256 para arquivos 7z e Zip 2.0 legado para arquivos Zip.";
+$content_info_text3 = "Se os arquivos forem grandes demais e não couberem no e‑mail ou servidor, basta dividi‑los.<br />Não se preocupe, depois de extraí‑los, eles serão iguais aos originais. :)";
+$content_info_compression = "O Keka pode criar arquivos nestes formatos:";
+$content_info_extraction = "E extrair todos estes formatos:";
+
+// Main content of the page
+$content_context_menu = "Menu contextual";
+
+// Changelog info
+$content_changelog_title = "Acompanhe toda a evolução do Keka";
+$content_changelog_entry_title = "Mudanças na versão";
+$content_changelog_firstpublic = "Primeiro lançamento público:";
+
+// 404
+$content_404 = "A página não pôde ser encontrada";
+$content_404_start = "Visite a <a href=\"https://www.keka.io/pt\">página principal</a>.";
+$content_404_more = "Talvez você queira ↓";
+
+// Bottom info
+$content_bottom_copying = 'aone e <a href="https://brlingo.com">BR Lingo</a>. Todos os direitos reservados.';
+
+?>
