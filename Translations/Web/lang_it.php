cc5919206033b0011f9997dd8bb2c0abff194c55
aone <aone.jga@gmail.com>
2018-06-19 09:57:23 +0200

    Italian web translation by VinBoiSoft
    http://vinboisoft.altervista.org

---
+++ Translations/Web/lang_it.php
@@ -0,0 +1,69 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: Italian
+// Translated by VinBoiSoft (vinboisoft@yahoo.it)
+//
+//
+
+// General
+$content_download = "Scarica";
+$content_issues = "Problemi";
+$content_help = "Aiuto";
+$content_forum = "Forum";
+$content_changelog = "Evoluzione";
+$content_title = "il compressore di macOS";
+
+// Download
+$content_plataform = "Richiede $version_replace o superiore";
+$content_download_text = "Memorizza di più, condividi con la privacy";
+$content_download_love = "Ti piace?";
+$content_download_legacy = "Legacy";
+
+// Like
+$content_donation_button = "Donazione";
+$content_donation_title = 'Se ti piace Keka, dagli un po\' di <i class="fa fa-heart" aria-hidden="true"></i>,<br />acquistalo su App Store o invia una donazione tramite PayPal!';
+$content_donation_mas = "Se acquisti Keka dall'App Store, sosterrai lo sviluppo,<br />l'app è la stessa versione di questo sito Web ma sarà aggiornata sempre attraverso l'App Store.";
+$content_donation_paypal = "Se non ti piace il Mac App Store o semplicemente vuoi divertirti con Keka<br />ma ti piace anche il progetto e vuoi dare il tuo supporto, puoi inviare una donazione utilizzando PayPal.";
+
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Versione beta di Keka";
+$content_beta_text = "Puoi testare le nuove funzionalità di Keka prima che vengano rilasciate.<br />Se trovi un bug o hai qualcosa da segnalare, vai su ";
+$content_beta_unavailable = "Non ci sono beta disponibili al momento.";
+$content_beta_up = "Ottieni l'ultima versione";
+	
+
+// Legacy
+$content_plataform_legacy =  "Per $version_replace";
+$content_legacy_title = "Versioni precedenti di Keka";
+$content_legacy_text = 'Nel corso degli anni il tuo Mac diventerà più vecchio e non supporterà<br />l\'ultima versione di Keka, ma tranquillo <i class="fa fa-coffee" aria-hidden="true"></i>, le vecchie versioni saranno sempre disponibili qui.';
+
+// Info
+$content_info_title1 = "Così semplice, così forte";
+$content_info_title2 = "La privacy è molto importante";
+$content_info_title3 = "Ancora troppo grande...";
+$content_info_text1 = "Non è nemmeno necessario aprire Keka per comprimere un file, tienilo nel tuo dock e utilizzalo da lì.<br />Basta trascinare e rilasciare i file e le cartelle sull'icona del Dock o sulla finestra di Keka per creare una versione meno pesante di essi.";
+$content_info_text2 = "Condividi in sicurezza semplicemente impostando una password e creando file altamente crittografati.<br />Keka utilizza le specifiche AES-256 per la crittografia dei tuoi file 7z e<br />le specifiche Zip 2.0 legacy per i tuoi file Zip.";
+$content_info_text3 = "Se i file sono davvero enormi e non si adattano alla tua e-mail o al tuo server, basta dividerli in pezzi.<br />Non ti preoccupare, si ricongiungeranno nuovamente per diventare il tuo file originale :)";
+$content_info_compression = "Keka può creare archivi in questi formati:";
+$content_info_extraction = "Ed estrarre tutti questi formati:";
+
+// Main content of the page
+$content_context_menu = "Menu contestuale";
+
+// Changelog info
+$content_changelog_title = "Dai un'occhiata all'evoluzione di Keka";
+$content_changelog_entry_title = "Modifiche nella versione";
+$content_changelog_firstpublic = "Primo rilascio pubblico:";
+
+// 404
+$content_404 = "La pagina non può essere trovata";
+$content_404_start = "Puoi andare alla <a href=\"https://www.keka.io/it\">pagina iniziale</a>,";
+$content_404_more = "o forse stavi cercando questo?";
+
+// Bottom info
+$content_bottom_copying = 'Tutti i diritti riservati. Tradotto da <a href="http://vinboisoft.altervista.org/">VinBoiSoft</a>.';
+
+?>
