f3b26cd83dbed59f137e26884af83c2992480e86
aone <aone.jga@gmail.com>
2018-07-02 11:09:08 +0200

    Bulgarian web translation by @deyanm

---
+++ Translations/Web/lang_bg.php
@@ -0,0 +1,69 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: English
+// Translated by aone (aone@keka.io)
+//
+//
+
+// General
+$content_download = "Свали";
+$content_issues = "Проблеми";
+$content_help = "Помощ";
+$content_forum = "Форум";
+$content_changelog = "Промени";
+$content_title = "macOS файлов архиватор";
+
+// Download
+$content_plataform = "Изисква $version_replace или по-нова";
+$content_download_text = "Съхрани повече, сподели поверително";
+$content_download_love = "Харесва ли ти?";
+$content_download_legacy = "Стари версии";
+
+// Like
+$content_donation_button = "Направи дарение";
+$content_donation_title = 'Ако харесваш Keka, просто й дай малко <i class="fa fa-heart" aria-hidden="true"></i>,<br />свали от App Store или дари с PayPal!';
+$content_donation_mas = "Ако купиш Keka от App Store, ще поддържаш разработката,<br />приложението е същото, като това от този уебсайт, само подновявано от App Store.";
+$content_donation_paypal = "Ако не харесваш MAS (Mac App Store) или искаш да си поиграеш с Keka<br />и също така харесваш проекта и искаш да дадеш малко любов, можеш да дариш с PayPal.";
+
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Beta версия на Keka";
+$content_beta_text = "Можеш да изпробваш най-новите възможности на Keka преди да са пуснати.<br />Ако си открил проблем или искаш нещо да докладваш, отиди на ";
+$content_beta_unavailable = "В момента няма налична beta версия.";
+$content_beta_up = "Свали последната версия";
+
+
+// Legacy
+$content_plataform_legacy =  "За $version_replace";
+$content_legacy_title = "Стари версии на Keka";
+$content_legacy_text = 'През годините твоят Mac ще остарява и няма да поддържа<br />най-новата Keka, но се успокой <i class="fa fa-coffee" aria-hidden="true"></i>, старите версии ще бъдат тук.';
+
+// Info
+$content_info_title1 = "Толкова просто, толкова силно";
+$content_info_title2 = "Поверителността е много важна";
+$content_info_title3 = "Все още е твърде голямо...";
+$content_info_text1 = "Ти дори няма нужда да отваряш Keka, за да компресираш файл, запази я в Dock и я използвай от там.<br />Просто провлачи твоите файлове и папки върху Dock иконата или Keka прозореца, за да направиш по-малка тяхна версия.";
+$content_info_text2 = "Споделяй сигурно само с парола и създавай криптирани файлове.<br />Използвайки AES-256 криптиране за твоите 7z файлове и<br />Zip 2.0 legacy криптиране за твоите Zip файлове.";
+$content_info_text3 = "Ако файловете са наистина големи и не се събират на твоят имейл или сървъв, просто ги раздели на парчета.<br />Не се притеснявай, те все още могат да се обединят в един :)";
+$content_info_compression = "Keka може да създава файлове в следните формати:";
+$content_info_extraction = "И разархивирай всички тези формати:";
+
+// Main content of the page
+$content_context_menu = "Контекстово меню";
+
+// Changelog info
+$content_changelog_title = "Погледни еволюцията на Keka";
+$content_changelog_entry_title = "Промени във версия";
+$content_changelog_firstpublic = "Първата публична версия:";
+
+// 404
+$content_404 = "Страницата не може да бъде намерена";
+$content_404_start = "Можете да отидете на <a href=\"https://www.keka.io\">началната страница</a>,";
+$content_404_more = "или искаш това?";
+
+// Bottom info
+$content_bottom_copying = 'Всички права запазени. Превод на български: <a href="https://github.com/deyanm">Деян Маринов</a>';
+
+?>
