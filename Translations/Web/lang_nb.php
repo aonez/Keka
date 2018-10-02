b862f08bc3848c94ecf2617731b002d49bb29f15
Patrick Sletvold <16patsle@users.noreply.github.com>
2018-10-02 20:25:52 +0200

    Translate website to nb

---
+++ Translations/Web/lang_nb.php
@@ -0,0 +1,57 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+//
+//
+// Language: Norwegian Bokmål
+// Translated by 16patsle
+//
+//
+// General
+$content_download = "Last ned";
+$content_issues = "Feil";
+$content_help = "Hjelp";
+$content_forum = "Forum";
+$content_changelog = "Endringslogg";
+$content_title = "fil-arkivereren for macOS";
+// Download
+$content_plataform = "Krever $version_replace eller nyere";
+$content_download_text = "Arkiver mer, del med personvern";
+$content_download_love = "Liker du det?";
+$content_download_legacy = "Foreldet";
+// Like
+$content_donation_button = "Doner";
+$content_donation_title = 'Hvis du liker Keka, bare gi det litt <i class="fa fa-heart" aria-hidden="true"></i>,<br />skaff det fra App Store eller send driks med PayPal!';
+$content_donation_mas = "Hvis du kjøper Keka på App Store, støtter du videre utvikling,<br />appen er den samme som versjonen du får via denne nettsiden, bare oppdatert gjennom App Store.";
+$content_donation_paypal = "Hvis du ikke liker MAS eller bare vile prøve ut Keka,<br />men også liker prosjektet og ønsker å støtte det, kan du sende driks via PayPal.";
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Beta-versjonen av Keka";
+$content_beta_text = "Du kan teste de nyeste funksjonene til Keka før de blir lansert.<br />Hvis du oppdager en feil, eller ønsker å rapportere noe annet, gå til ";
+$content_beta_unavailable = "Det er ikke noen beta-versjon tilgjengelig for øyeblikket.";
+$content_beta_up = "Skaff nyeste versjon";
+	
+// Legacy
+$content_plataform_legacy =  "For $version_replace";
+$content_legacy_title = "Foreldete versjoner av Keka";
+$content_legacy_text = 'Med tiden vil din Mac bli eldre, og vil ikke støtte<br /> den nyeste versjonen av Keka, men slapp av <i class="fa fa-coffee" aria-hidden="true"></i>, de gamle versjonene vil være tilgjengelig her.';
+// Info
+$content_info_title1 = "Så enkelt, så kraftig";
+$content_info_title2 = "Personvern er veldig viktig";
+$content_info_title3 = "Fortsatt for stor...";
+$content_info_text1 = "Du trenger ikke engang åpne Keka for å komprimere en fil, behold Keka i dokken din og bruk det derfra.<br />Bare dra og slipp filene og mappene dine på Dock-ikonet eller et Keka-vindu for å opprette en mindre versjon av dem.";
+$content_info_text2 = "Del trygt ved kun å sette et passord og opprette sterkt komprimerte filer.<br />Keka bruker AES-256-kryptering for 7z-filene dine og<br />Zip 2.0 legacy-kryptering for Zip-filene.";
+$content_info_text3 = "Hvis filene er veldig store, og ikke får plass i en epost eller på en server, bare splitt dem i flere biter.<br />Slapp av, de vil fortsatt pakkes ut til originalfilen :)";
+$content_info_compression = "Keka kan opprette filer i disse formatene:";
+$content_info_extraction = "Og pakke ut alle disse formatene:";
+// Main content of the page
+$content_context_menu = "Kontekstmeny";
+// Changelog info
+$content_changelog_title = "Ta en titt på Kekas utvikling";
+$content_changelog_entry_title = "Endringer i versjon";
+$content_changelog_firstpublic = "Første lanserte versjon:";
+// 404
+$content_404 = "Siden ble ikke funnet";
+$content_404_start = "Du kan gå til <a href=\"https://www.keka.io\">forsiden</a>,";
+$content_404_more = "eller kanskje du vil dette?";
+// Bottom info
+$content_bottom_copying = 'Alle rettigheter forbeholdt.';
+?>
