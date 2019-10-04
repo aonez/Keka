fc4595b53883573923111c8fa6ef688ce5833ea0
Master Wolf <1234hdpa@gmail.com>
2019-10-04 19:55:13 +0700

    Add Vietnames language for Website
    + Add Vietnamese language for Website.
    + Add myself to translators list.
    
    Any spelling and grammar corrections are welcome! :D

---
+++ Translations/Web/lang_vn.php
@@ -0,0 +1,70 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: Vietnamese
+$content_language_locale = "vn";
+// Translated by 1234hdpa (1234hdpa@gmail.com)
+//
+//
+
+// General
+$content_download = "Tải về";
+$content_issues = "Vấn đề";
+$content_help = "Giúp đỡ";
+$content_forum = "Diễn đàn";
+$content_changelog = "Lịch sử cập nhật";
+$content_title = "trình nén và giải nén cho macOS";
+
+// Download
+$content_plataform = "Yêu cầu $version_replace hoặc mới hơn";
+$content_download_text = "Lưu trữ nhiều hơn, chia sẻ riêng tư hơn";
+$content_download_love = "Bạn thích nó chứ?";
+$content_download_legacy = "Phiên bản cũ";
+
+// Like
+$content_donation_button = "Quyên góp";
+$content_donation_title = 'Nếu bạn thích Keka, hãy gửi nó một chút <i class="fa fa-heart" aria-hidden="true"></i>, <br />tải nó từ App Store hoặc gửi một chút tiền ủng hộ đến PayPal!';
+$content_donation_mas = "Nếu bạn mua Keka từ App Store, bạn sẽ đóng góp vào việc phát triển ứng dụng,<br />ứng dụng có cùng phiên bản như trên trang web này nhưng sẽ luôn được cập nhật thông qua App Store.";
+$content_donation_paypal = "Nếu bạn không thích Cửa hàng App Store hoặc chỉ muốn dạo chơi với Keka<br />nhưng cũng yêu mến dự án này và muốn ủng hộ nó, bạn có thể gửi tiền ủng hộ thông qua Paypal.";
+
+// Beta
+$content_plataform_beta = "Beta";
+$content_beta_title = "Phiên bản Beta của Keka";
+$content_beta_text = "Bạn có thể chạy thử tính năng mới của Keka trước khi nó được phát hành chính thức.<br />Nếu bạn phát hiện lỗi hay muốn góp ý với chúng tôi, vui lòng truy cập vào ";
+$content_beta_unavailable = "Hiện tại không có phiên bản beta nào.";
+$content_beta_up = "Get the latest version";
+	
+
+// Legacy
+$content_plataform_legacy =  "Dành cho $version_replace";
+$content_legacy_title = "Phiên bản cũ của Keka";
+$content_legacy_text = 'Dần dần theo năm tháng, Mac của bạn sẽ già đi và sẽ không còn hỗ trợ<br />Keka mới nhất, nhưng đừng lo lắng <i class="fa fa-coffee" aria-hidden="true"></i>, những phiên bản cũ hơn vẫn sẽ được lưu tại đây.';
+
+// Info
+$content_info_title1 = "Đơn giản nhưng mạnh mẽ";
+$content_info_title2 = "Tính riêng tư rất quan trọng";
+$content_info_title3 = "Vẫn quá to...";
+$content_info_text1 = "Bạn không cần phải mở Keka để nén một tập tin, giữ nó trên thanh ứng dụng và sử dụng ngay trên đó.<br />Chỉ cần kéo và thả tập tin và thư mục của bạn xuống thanh ứng dụng hoặc cửa sổ Keka để tạo ra phiên bản nhỏ gọn cho các tập tin và thư mục đó.";
+$content_info_text2 = "Chia sẻ an toàn khi đặt mật khẩu và tạo các tập tin được mã hoá cao cấp.<br />Sử dụng mã hoá AES-256 tuỳ chỉnh cho tập tin 7z của bạn và<br />tuỳ chỉnh mã hoá Zip 2.0 cho tập tin Zip của bạn.";
+$content_info_text3 = "Nếu các tập tin quá lớn và không để nhét vừa email hay máy chủ, chỉ cần chia nhỏ chúng thành nhiều mảnh.<br />Đừng lo, chúng vẫn sẽ bảo lưu nguyên vẹn tập tin gốc của bạn :)";
+$content_info_compression = "Keka có thể tạo tập tin theo các định dạng sau:";
+$content_info_extraction = "Và giải nén tất cả các định dạng sau:";
+
+// Main content of the page
+$content_context_menu = "Danh mục";
+
+// Changelog info
+$content_changelog_title = "Cùng nhìn lại sự tiến hoá của Keka";
+$content_changelog_entry_title = "Những thay đổi theo phiên bản";
+$content_changelog_firstpublic = "Lần đầu tiên phát hành chính thức:";
+
+// 404
+$content_404 = "Không thể tìm thấy trang này";
+$content_404_start = "Bạn có thể truy cập vào <a href=\"https://www.keka.io\">trang bắt đầu</a>,";
+$content_404_more = "hay bạn muốn nhìn 404 hoài nhỉ?";
+
+// Bottom info
+$content_bottom_copying = 'All rights reserved.';
+
+?>
