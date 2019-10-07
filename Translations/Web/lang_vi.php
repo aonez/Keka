73b6a1e331a8bc57e3099858e9084e1d6ba7f752
aonez <aone.jga@gmail.com>
2019-10-07 22:42:20 +0200

    Vietnamese translation should use VI, not VN

--- Translations/Web/lang_vi.php
+++ Translations/Web/lang_vi.php
@@ -7,6 +7,11 @@ $content_language_locale = "vn";
 // Translated by 1234hdpa (1234hdpa@gmail.com)
 //
 //
+// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
+$show_content_bottom_translator = false;
+$content_bottom_translator = 'Translated by <a href="https://yoursite.or.email.or.whatever">Yourself</a>.';
+//
+//
 
 // General
 $content_download = "Tải về";
