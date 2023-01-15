5d297d45fc98ba4b284c82d78aadd36b8aad0f2a
aone <aone.jga@gmail.com>
2023-01-15 17:29:37 +0100

    Catalan web translation

---
+++ Translations/Web/lang_ca.php
@@ -0,0 +1,86 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: Catalan
+$content_language_locale = "ca";
+// Translated by Markus (marcgasensio@icloud.com)
+//
+//
+// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
+$show_content_bottom_translator = true;
+$content_bottom_translator = 'Translated by <a href="https://discord.com/invite/PX2e2UeMau">Markus</a>.';
+//
+//
+
+// General
+$content_download = "Baixa la";
+$content_issues = "Reporta";
+$content_help = "Ajuda";
+$content_forum = "Fòrum";
+$content_changelog = "Evolució";
+$content_title = "el compressor de macOS";
+
+// Download
+$content_platform = "Requereix $version_replace o superior";
+$content_download_text = "Emmagatzema més, comparteix amb privacitat";
+$content_download_love = "T'agrada?";
+$content_download_legacy = "Antigues";
+$content_download_helper = "Ajudant";
+
+// Like
+$content_donation_button = "Donar";
+$content_donation_title = 'Si t\'agrada el Keka dona-li una mica de <i class="fa fa-heart" aria-hidden="true"></i>, <br />compra\'l en l\'App Store o envia un donatiu via PayPal!';
+$content_donation_mas = "Si compres el Keka a través de l'App Store, tindràs la mateixa versió que a la web<br />però sempre s'actualitzara a través de l'App Store.";
+$content_donation_paypal = "Si no t'agrada l'App Store o senzillament vols jugar amb les entranyes del Keka<br />però a més t'agrada el projecte i vols donar-li el teu suport, pots enviar una donació utilitzant PayPal.";
+
+// Beta
+$content_platform_beta = "Beta";
+$content_beta_title = "Versió beta del Keka";
+$content_beta_text = "Pots provar les novetats del Keka abans de què arribin a la versió estable.
+<br />Si trobes cap problema o tens alguna cosa a comentar, dirigeix-te a ";
+$content_beta_unavailable = "No hi ha versions beta disponibles";
+$content_beta_up = "Descarregat l'última versió.";
+
+// Legacy
+$content_platform_legacy =  "Per $version_replace";
+$content_legacy_title = "Versions anteriors del Keka";
+$content_legacy_text = 'Amb el pas del temps, el teu Mac es fa gran i potser ha deixat de suportar <br />l\'última versió del Keka, però tranquil <i class="fa fa-coffee" aria-hidden="true"></i>, aquestes sempre estaran amb tu.';
+
+// Info
+$content_info_title1 = "Tan senzill, tan potent";
+$content_info_title2 = "La privacitat és molt important";
+$content_info_title3 = "Segueix sent massa gran?";
+$content_info_text1 = "Ni tan sols has d'obrir el Keka per comprimir un arxiu, manteniu-lo en el teu Dock i utilitza'l des de allà.<br />Senzillament arrossega i deixa anar els arxius i carpetes a sobre de la icona o en la pestanya del Keka per generar una versió més lleugera d'ells.";
+$content_info_text2 = "Tan sols una contrasenya podrà crear arxius molt encriptats que podràs compartit amb tota seguretat.<br />El Keka utilitza l'especificació AES-256 per l'encriptació dels teus fitxers 7z<br />i Zip 2.0 legacy pels teus fitxers Zip.";
+$content_info_text3 = "Si els teus arxius són desmesuradament grans i no caben en el teu correu o servidor, senzillament divideix-los en fragments.<br />No has de preocupar-te, tornaran a ajuntar de nou per convertir-se en el teu arxiu original :)";
+$content_info_compression = "Amb el Keka pots crear arxius utilitzant aquests formats:";
+$content_info_extraction = "I extraure tots aquests formats:";
+
+// Default app
+$content_defaultapp_title = "Establir el Keka com el descompressor predeterminat";
+$content_defaultapp_text = 'El Keka <a href="https://github.com/aonez/Keka/wiki/Default-application">necessita un ajudant <i class="far fa-question-circle"></i></a> per establir com l\'aplicació per defecte. <br />Senzillament descarrega, extrau, mou a l\'escriptori i executa l\'ajudant.';
+
+// Privacy Policy
+$content_privacypolicy_title = 'Política de privacitat';
+
+// Terms of use
+$content_termsofuse_title = 'Termes d\'ús';
+
+// Main content of the page
+$content_context_menu = "Menú contextual";
+
+// Changelog info
+$content_changelog_title = "Dona un cop d'ull a l'evolució del Keka";
+$content_changelog_entry_title = "Canvis en la versió";
+$content_changelog_firstpublic = "Primera versió publica:";
+
+// 404
+$content_404 = "No s'ha trobat el que cercaves";
+$content_404_start = "Pots anar a la <a href=\"https://www.keka.io/\">pàgina inicial</a>";
+$content_404_more = "o potser volies això?";
+
+// Bottom info
+$content_bottom_copying = "Tots els drets reservats.";
+
+?>
