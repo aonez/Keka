7c4352c6c79d57d7a2743efbf4e63d7dbca38820
zetenynagy <40546076+zetenynagy@users.noreply.github.com>
2023-11-27 22:04:44 -0600

    Hungarian translation added for website and iOS version
    Added Hungarian translation for the website and the iOS app version. iOS app might need testing for a better understanding of context.

---
+++ Translations-Web/lang_hu.php
@@ -0,0 +1,115 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: Hungarian
+$content_language_locale = "hu";
+// Translated by Zétény Nagy
+//
+//
+// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
+$show_content_bottom_translator = true;
+$content_bottom_translator = 'Translated by <a href="https://zetenynagy.com">Zétény Nagy</a>.';
+//
+//
+
+// iOS
+$ios_title = "$keka iOS-re";
+$ios_content_title = "az iOS tömörítő";
+
+// General
+$content_download = "Letöltés";
+$content_issues = "Problémák";
+$content_help = "Segítség";
+$content_forum = "Beszélgetések";
+$content_changelog = "Változáslista";
+$content_title = "a macOS tömörítő";
+
+// Download
+$content_platform = "Legalább $version_replace szükséges";
+$content_download_text = "Tárolj többet, ossz meg biztonságban";
+$content_download_love = "Tetszik?";
+$content_download_legacy = "Régebbi verziók";
+$content_download_helper = "Segítő";
+$content_downloading = "Letöltés folyamatban: ";
+$content_get_platform = "Szerezd meg erre is: ";
+$content_testflight = "Ennek a verziónak a telepítéséhez TestFlight szükséges";
+$content_downloading_if_fails = "Ha a letöltés nem indul el automatikusan, ";
+$content_downloading_if_fails_click_here = "kattints ide";
+
+// Like
+$content_donation_button = "Adományozz";
+$content_donation_title = 'Ha tetszik a Keka, mutasd ki! <i class="fa fa-heart" aria-hidden="true"></i> <br />Szerezd meg az App Store-ból, vagy küldj borravalót PayPal-on!';
+$content_donation_mas = "Ha a Kekát az App Store-ból szerzed be, támogatod a fejlesztést. <br />Az alkalmazás ugyanaz a verzió, mint ami ezen a weboldalon elérhető, csak az App Store-on keresztül frissül.";
+$content_donation_paypal = "Ha nem kedveled a Mac App Store-t, vagy csak ki szórakozni szeretnél a Kekával,<br />de tetszik a projekt és ki szeretnéd mutatni, tudsz borravalót küldeni a PayPal-on keresztül.";
+
+// Beta
+$content_platform_beta = "Béta";
+$content_beta_title = "A Keka béta verziója";
+$content_beta_text = "Kipróbálhatod a Keka legújabb funkcióit mielőtt kiadásra kerülnek.<br />Ha hibát észlelsz, vagy jelezni szeretnél valamit, itt teheted: ";
+$content_beta_unavailable = "Jelenleg nincs elérhető béta.";
+$content_beta_up = "Szerezd be a legújabb verziót";
+
+// Legacy
+$content_platform_legacy =  "$version_replace-hoz";
+$content_legacy_title = "A Keka régebbi verziói";
+$content_legacy_text = 'Az évek során a Mac-ed megöregszik, és nem fogja támogatni <br />a legújabb Kekát, de semmi pánik <i class="fa fa-coffee" aria-hidden="true"></i>, a régi verziók itt elérhetőek.';
+
+// Info
+$content_info_title1 = "Olyan egyszerű, olyan erőteljes";
+$content_info_title2 = "Az adatvédelem nagyon fontos";
+$content_info_title3 = "Még mindig túl nagy...";
+$content_info_text1 = "Meg sem kell nyitnod a Kekát, hogy tömöríts egy fájlt: tartsd a Dock-ban, és használd onnan.<br />Csak húzd a fájljaidat és mappáidat a Dock-ban az ikonra, vagy a Keka ablakba, hogy lekicsinyítsd őket.";
+$content_info_text2 = "Ossz meg biztonságosan: csak állíts be egy jelszót és készíts magas fokon biztosított fájlokat.<br />Az AES-256 titkosítási szabványt használjuk a 7z fájljaidhoz, és<br />Zip 2.0 legacy titkosítási szabványt a Zip fájlokhoz.";
+$content_info_text3 = "Ha a fájlok hatalmasak lesznek, és nem férnek a leveledbe vagy a szerveredre, csak szedd őket darabokra.<br />Ne aggódj, újra megnőnek, hogy összeálljanak az eredeti fájloddá :)";
+$content_info_compression = "A Keka ilyen formátumú fájlokat tud készíteni:";
+$content_info_extraction = "És ilyen formátumú fájlokat tud kibontani:";
+
+// Info v2 (iOS and future macOS)
+$content_info_v2_title1 = "Tárolj többet";
+$content_info_v2_title2 = "Ossz meg biztonságban";
+$content_info_v2_title3 = "Egy tökéletes böngészési élmény";
+$content_info_v2_title4 = "Több feladat egyszerre";
+$content_info_v2_title5 = "Mindig kéznél";
+$content_info_v2_text1 = "Többféle tömörítési formátumma,l<br />hogy megtaláld azt, ami tökéletes";
+$content_info_v2_text2 = "Védd meg a fájljaidat jelszavakkal,<br />és titkosíts őket AES-256-tal";
+$content_info_v2_text3 = "Előnézet, Kibontás és Megosztás<br />amikor szükséged van rá";
+$content_info_v2_text4 = "Kibontás, Tömörítés és Böngészés<br />határok nélkül";
+$content_info_v2_text5 = "Kibontás, Tömörítés és Böngészés<br />bárhonnan, a Megosztási Műveletek segítségével";
+
+// Default app
+$content_defaultapp_title = "A Keka beállítása alapértelmezett kibontási alkalmazásként";
+$content_defaultapp_text = 'A Kekának <a href="https://github.com/aonez/Keka/wiki/Default-application">egy segítőre <i class="far fa-question-circle"></i></a> van szüksége, hogy be tudd állítani alapértelmezettként.<br />Egyszerűen töltsd le, bontsd ki, rakd az Íróasztalra, és nyisd meg.';
+
+// Terms of use
+$content_termsofuse_title = 'Használati feltételek (angolul)';
+$content_termsofuse_disclaimer = 'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
+$content_termsofuse_inapp_title = 'Alkalmazáson belüli vásárlások';
+$content_termsofuse_inapp = 'A Keka App Store-os verziója alkalmazáson belüli vásárlásokat kínál egy módként a fejlesztés megsegítésére és borravaló hagyására, hasonlóan, mint a <a href="https://www.keka.io/#lovekeka">PayPal/Szponzorok a weboldalon</a>. Ezek a vásárlások választhatóak, és nem adnak semmiféle új funkciót.';
+
+// Privacy Policy
+$content_privacypolicy_title = 'Adatvédelmi nyilatkozat';
+$content_privacypolicy_content = 'A Keka nem küld vagy tárol semmiféle adatot a számítógépeden.';
+$content_privacypolicy_MAS = 'A Mac App Store verzió nem rendelkezik semmilyen hálozati funkcionalitással.';
+$content_privacypolicy_WEB = 'A webes változat a Sparkle-t használja a frissítési folyamathoz (amit le lehet tiltani), ezért ez a komponens hozzáférhet az Internethez. Ez csak az új verzió ellenőrzésére és letöltésére szolgál. Ez a komponens soha sem tárol vagy oszt meg semmilyen információt, még a <a href="https://sparkle-project.org/documentation/system-profiling/">névtelen rendszeradatokat</a> sem, amely a Keka néhány 1.0.0 előtti verziójában engedélyezve volt, de sosem volt használva.';
+
+// Main content of the page
+$content_context_menu = "Kontextusmenü";
+
+// Changelog info
+$content_changelog_title = "Nézd meg közelebbről a Keka fejlődését";
+$content_changelog_entry_title = "Változások ebben a verzióban: ";
+$content_changelog_firstpublic = "Az első publikus kiadás:";
+
+// 404
+$content_404 = "Az oldal nem található";
+$content_404_start = "Itt találod a <a href=\"https://www.keka.io\">kezdőoldalt</a>";
+$content_404_more = "vagy ezt keresed?";
+
+// Maintenance
+$content_maintenance = "Nemsokára visszatérünk";
+
+// Bottom info
+$content_bottom_copying = 'Minden jog fenntartva.';
+
+?>
