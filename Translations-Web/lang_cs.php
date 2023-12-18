c8cc040d58e806c15c88dc9e2cd43aac8c030657
František Erben <erben.fr@gmail.com>
2023-12-18 23:52:23 +0100

    Add Czech localizalization for Web

---
+++ Translations-Web/lang_cs.php
@@ -0,0 +1,115 @@
+<?php // Keka lang - version 0.3 - Started on 26/11/09 - by aone
+
+//
+//
+// Language: English
+$content_language_locale = "cs";
+// Translated by aone (erben.fr@gmail.com)
+//
+//
+// Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
+$show_content_bottom_translator = false;
+$content_bottom_translator = 'Přeloženo <a href="mailto:">Františkem Erbenem</a>.';
+//
+//
+
+// iOS
+$ios_title = "$keka pro iOS";
+$ios_content_title = "archivační pomůcka pro iOS";
+
+// General
+$content_download = "Zkopírovat";
+$content_issues = "Problémy";
+$content_help = "Nápověda";
+$content_forum = "Diskuse";
+$content_changelog = "Vývojové změny";
+$content_title = "archivační pomůcka pro macOS";
+
+// Download
+$content_platform = "Vyžaduje $version_replace nebo novější";
+$content_download_text = "Ukládejte více, sdílejte bezpečně";
+$content_download_love = "Líbí se Vám?";
+$content_download_legacy = "Starší verze";
+$content_download_helper = "Pomocník";
+$content_downloading = "Kopíruje se ";
+$content_get_platform = "Získat pro ";
+$content_testflight = "Pro instalaci této verze je potřeba TestFlight";
+$content_downloading_if_fails = "Pokud kopírování nezačne automaticky ";
+$content_downloading_if_fails_click_here = "klepněte zde";
+
+// Like
+$content_donation_button = "Přispět";
+$content_donation_title = 'Pokud se Vám aplikace Keka líbí můžete ji ohodnotit <i class="fa fa-heart" aria-hidden="true"></i>, <br />stáhnout si ji z App Store nebo poslat drobný obnos přes PayPal!';
+$content_donation_mas = "Pokud si zakoupíte aplikaci Keka z App Store podpoříte tím její vývoj,<br />aplikace je zcela stejná jako verze z tohoto webu pouze aktualizace probíhají pomocí App Store.";
+$content_donation_paypal = "Pokud nemáte rádi App Store nebo se chcete s aplikací Keka jen pohrát,<br />ale projekt se Vám přesto líbí a chcete ho podpořit, můžete poslat drobný obnos přes PayPal.";
+
+// Beta
+$content_platform_beta = "Beta";
+$content_beta_title = "Zkušební verze Keka";
+$content_beta_text = "Můžete testovat nejnovější vlastnosti Keka před oficiálním zveřejněním.<br />Pokud najdete chybu nebo chcete nahlásit neobvyklé chování, přejděte na ";
+$content_beta_unavailable = "Nyní není k dispozici žádná zkušební verze.";
+$content_beta_up = "Získat nejnovější verzi";
+
+// Legacy
+$content_platform_legacy =  "Pro $version_replace";
+$content_legacy_title = "Starší verze aplikace Keka";
+$content_legacy_text = 'V průběhu let váš Mac stárne a nebude podporovat<br />poslední verzi aplikace Keka, ale klid <i class="fa fa-coffee" aria-hidden="true"></i>, starší verze naleznete zde.';
+
+// Info
+$content_info_title1 = "V jednoduchosti je síla";
+$content_info_title2 = "Vaše soukromí je pro nás důležité";
+$content_info_title3 = "Stále příliš velké…";
+$content_info_text1 = "Kvůli komprimaci souboru ani nemusíte otevírat aplikaci Keka ani ji udržovat v Doku.<br />Prostě přetáhněte soubory a složky na ikonu aplikace Keka v Doku nebo na okno otevřené aplikace.";
+$content_info_text2 = "Sdílejte soubory bezpečněji, chraňte je heslem a vytvářejte zašifrované soubory.<br />Používejte šifrovací metodu AES-256 pro vaše archivy 7z<br />a starší metody šifrování Zip 2.0 pro vaše Zip archivy.";
+$content_info_text3 = "Pokud jsou soubory skutečně velké a přesahují limity e-mailu, prostě je rozdělte na více částí.<br />Nemějte obavy stále půjdou spojit a dekomprimovat do původní podoby :)";
+$content_info_compression = "Keka umí vytvářet tyto typy archivů:";
+$content_info_extraction = "Keka umí rozbalit tyto typy archivů:";
+
+// Info v2 (iOS and future macOS)
+$content_info_v2_title1 = "Ukládejte více";
+$content_info_v2_title2 = "Sdílejte bezpečně";
+$content_info_v2_title3 = "Perfektní zážitek z prohlížení";
+$content_info_v2_title4 = "Více úlohové zpracování";
+$content_info_v2_title5 = "Vždy po ruce";
+$content_info_v2_text1 = "S větším výběrem komprimačních formátů<br />si vyberete ten nejlepší";
+$content_info_v2_text2 = "Zabezpečte vaše sdílené soubory heslem<br />a šifrováním AES-256";
+$content_info_v2_text3 = "Náhled, dekomprimace a sdílení<br />pouze pokud je třeba";
+$content_info_v2_text4 = "Dekomprimace, komprimace a procházení<br />bez omezení";
+$content_info_v2_text5 = "Dekomprimace, komprimace a procházen<br />odkudkoliv pomocí akce Sdílení";
+
+// Default app
+$content_defaultapp_title = "Nastavte si aplikaci Keka jako výchozí pro práci s archivy";
+$content_defaultapp_text = 'Aplikace Keka <a href="https://github.com/aonez/Keka/wiki/Default-application">vyžaduje pomocnou aplikaci <i class="far fa-question-circle"></i></a> aby mohla být výchozí aplikací pro práci s archivy.<br />Jednoduše ji zkopírujte, rozbalte, přesuňte na Plochu a spusťte.';
+
+// Terms of use
+$content_termsofuse_title = 'Licence a Podmínky použití (Anglicky)';
+$content_termsofuse_disclaimer = 'THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
+$content_termsofuse_inapp_title = 'Nákupy v aplikaci';
+$content_termsofuse_inapp = 'Verze aplikace Keka pro App Store nabízí nákupy v aplikaci jako cestu k udržení podpory vývoje projektu, podobně jako <a href="https://www.keka.io/#lovekeka">PayPal/Sponzoring na webu</a>. Tato podpora je dobrovolná na nezaručuje vývoj nových vlastností aplikace.';
+
+// Privacy Policy
+$content_privacypolicy_title = 'Ochrana soukromí';
+$content_privacypolicy_content = 'Aplikace Keka neodesílá žádná uložená data z vašeho počítače.';
+$content_privacypolicy_MAS = 'Verze App Store nemá přístup síťovým rozhraním.';
+$content_privacypolicy_WEB = 'Webová verze používá pro aktualizaci Sparkle (lze vypnout), tato součást přistupuje k Internetu. Slouží výhradně k ověření dostupnosti nové verze a jejímu zkopírování. Tato součást nikdy neukládá ani nesdílí žádné informace, dokonce ani <a href="https://sparkle-project.org/documentation/system-profiling/">anonymní data profilu systému</a>, která byla povolena ve velmi ranných verzích (starší než 1.0.0), ale nikdy použita.';
+
+// Main content of the page
+$content_context_menu = "Kontextová nabídka";
+
+// Changelog info
+$content_changelog_title = "Prohlédněte si vývojové změny v aplikaci Keka";
+$content_changelog_entry_title = "Změny ve verzi";
+$content_changelog_firstpublic = "První veřejná verze:";
+
+// 404
+$content_404 = "Stránka nebyla nalezena";
+$content_404_start = "Můžete jít na <a href=\"https://www.keka.io\">domovskou stránku</a>";
+$content_404_more = "nebo jste si to přáli?";
+
+// Maintenance
+$content_maintenance = "Budeme brzo zpět";
+
+// Bottom info
+$content_bottom_copying = 'Všechna práva vyhrazena.';
+
+?>
