a0b3ceac5b31103167c766d6aa8f8dc53f8d064c
Alexey Ladygin <153652995+nitrodox@users.noreply.github.com>
2024-05-19 17:07:32 +0200

    Update lang_ru.php
    * Untranslated strings updated
    * Russian style (too many formal pronouns, word-to-word translation etc.)
    * Russian spelling (use of "ё/e" according to the official rules: https://therules.ru/vowels-4/)
    * Some terms are the same as in desktop version now
    * Non-breaking spaces added for better typography

--- Translations-Web/lang_ru.php
+++ Translations-Web/lang_ru.php
@@ -5,17 +5,18 @@
 // Language: Russian
 $content_language_locale = "ru";
 // Translated by Nikita Karamov (me@kytta.dev)
+// Translated by Alexey Ladygin (https://github.com/nitrodox)
 //
 //
 // Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
 $show_content_bottom_translator = true;
-$content_bottom_translator = "Перевёл <a href=\"https://www.kytta.dev/\">Никита Карамов</a>.";
+$content_bottom_translator = "Перевод: <a href=\"https://www.kytta.dev/\">Никита Карамов</a>, <a href=\"https://github.com/nitrodox\">Алексей Ладыгин</a>.";
 //
 //
 
 // iOS
 $ios_title = "$keka for iOS";
-$ios_content_title = "the iOS file archiver";
+$ios_content_title = "архиватор файлов для iOS";
 
 // General
 $content_download = "Скачать";
@@ -31,77 +32,77 @@ $content_download_text = "Храните больше, делитесь безо
 $content_download_love = "Понравилось?";
 $content_download_legacy = "Старые версии";
 $content_download_helper = "Helper";
-$content_downloading = "Downloading ";
-$content_get_platform = "Get it for ";
-$content_testflight = "TestFlight needed to install this build";
-$content_downloading_if_fails = "If the download does not start automatically ";
-$content_downloading_if_fails_click_here = "click here";
+$content_downloading = "Идет загрузка ";
+$content_get_platform = "Версия для ";
+$content_testflight = "Для установки этой сборки требуется TestFlight";
+$content_downloading_if_fails = "Если загрузка не&nbsp;началась автоматически ";
+$content_downloading_if_fails_click_here = "щелкните здесь";
 
 // Like
 $content_donation_button = "Пожертвовать";
-$content_donation_title = "Если вы <i class=\"fa fa-heart\" aria-hidden=\"true\"></i> Keka,<br />то купите её в App Store или пожертвуйте через PayPal!";
-$content_donation_mas = "Покупая Keka в App Store, вы поддерживаете разработчика,<br />приложение ничем не отличается от версии на сайте, только обновляется через App Store.";
-$content_donation_paypal = "Если вам не нравится MAS или вы просто пробуете Keka,<br />но хотите поддержать проект, вы можете пожертвовать деньги через PayPal.";
+$content_donation_title = "Если вы <i class=\"fa fa-heart\" aria-hidden=\"true\"></i> Keka,<br />то&nbsp;купите ее в&nbsp;App Store или пожертвуйте через PayPal!";
+$content_donation_mas = "Покупая Keka в&nbsp;App Store, вы поддерживаете разработчика.<br />Приложение ничем не&nbsp;отличается от&nbsp;версии на&nbsp;сайте, но&nbsp;обновляется через App Store.";
+$content_donation_paypal = "Если вам не&nbsp;нравится MAS или просто пробуете Keka,<br />но&nbsp;хотите поддержать проект, можете пожертвовать деньги через PayPal.";
 
 // Beta
 $content_platform_beta = "Бета";
 $content_beta_title = "Бета-версия Keka";
-$content_beta_text = "Вы можете протестировать новые возможности Keka перед релизом.<br />Если вы найдёте баг, следуйте в раздел ";
-$content_beta_unavailable = "На данный момент нет доступных бета-версий.";
+$content_beta_text = "Вы можете протестировать новые возможности Keka перед релизом.<br />Если найдете баг, перейдите в&nbsp;раздел ";
+$content_beta_unavailable = "На&nbsp;данный момент нет доступных бета-версий.";
 $content_beta_up = "Скачать новейшую версию";
 
 // Legacy
 $content_platform_legacy =  "Для $version_replace";
 $content_legacy_title = "Старые версии Keka";
-$content_legacy_text = "Рано или поздно ваш Mac устареет и не будет поддерживать<br />новые версии Keka, но не волнуйтесь <i class=\"fa fa-coffee\" aria-hidden=\"true\"></i>, старые версии будут здесь.";
+$content_legacy_text = "Рано или поздно ваш Mac устареет и&nbsp;не&nbsp;будет поддерживать<br />новые версии Keka, но&nbsp;не&nbsp;волнуйтесь <i class=\"fa fa-coffee\" aria-hidden=\"true\"></i>, старые версии будут здесь.";
 
 // Info
 $content_info_title1 = "Простая и мощная";
-$content_info_title2 = "Конфиденциальность — это важно";
-$content_info_title3 = "Всё ещё тяжело...";
-$content_info_text1 = "Даже не нужно открывать Keka, чтобы сжать файл, оставьте иконку в Dock и используйте оттуда.<br />Просто перетащите ваши файлы и папки на иконку в Dock или окно Keka, чтобы создать сжатую версию.";
+$content_info_title2 = "Конфиденциальность&nbsp;— это важно";
+$content_info_title3 = "Все еще тяжело...";
+$content_info_text1 = "Не&nbsp;нужно открывать Keka, чтобы сжать файл, оставьте иконку в&nbsp;Dock и пользуйтесь ей.<br />Просто перетащите ваши файлы и&nbsp;папки на&nbsp;иконку в&nbsp;Dock или в&nbsp;окно Keka, чтобы создать архив.";
 $content_info_text2 = "Передавайте данные более безопасно, создавая зашифрованные паролем архивы.<br />Используя шифрование AES-256 для файлов 7z и<br />шифрование Zip 2.0 для файлов Zip.";
-$content_info_text3 = "Если ваши файлы действительно огромны, и не помещаются в электронное письмо или на сервер, поделите их на части.<br />Не волнуйтесь, они вновь объединятся в исходный файл :)";
+$content_info_text3 = "Если ваши файлы действительно огромны и&nbsp;не&nbsp;помещаются в&nbsp;электронное письмо или на&nbsp;сервере, разделите их на&nbsp;части.<br />Не&nbsp;волнуйтесь, они вновь объединятся в&nbsp;исходный файл :)";
 $content_info_compression = "Keka может создавать файлы этих форматов:";
-$content_info_extraction = "И распаковывать эти форматы:";
+$content_info_extraction = "И&nbsp;распаковывать эти форматы:";
 
 // Info v2 (iOS and future macOS)
-$content_info_v2_title1 = "Store more";
-$content_info_v2_title2 = "Share with privacy";
-$content_info_v2_title3 = "A perfect Browsing experience";
-$content_info_v2_title4 = "Multitasking at its finest";
-$content_info_v2_title5 = "Always at hand";
-$content_info_v2_text1 = "With multiple Compression Formats<br />to choose the Perfect One";
-$content_info_v2_text2 = "Protect your shared files with a Password<br />and Encrypt them with AES-256";
-$content_info_v2_text3 = "Preview, Extract and Share<br />only what You Need";
-$content_info_v2_text4 = "Extract, Compress and Browse<br />without limits";
-$content_info_v2_text5 = "Extract, Compress and Browse<br />from anywhere using the Share Actions";
+$content_info_v2_title1 = "Храните больше";
+$content_info_v2_title2 = "Делитесь безопасно";
+$content_info_v2_title3 = "Удобная работа с&nbsp;файлами";
+$content_info_v2_title4 = "Впечатляющая многозадачность";
+$content_info_v2_title5 = "Всегда под рукой";
+$content_info_v2_text1 = "Выбирайте самый подходящий формат сжатия<br />среди множества доступных";
+$content_info_v2_text2 = "Защищайте файлы паролями<br />и&nbsp;используйте шифрование AES-256";
+$content_info_v2_text3 = "Просматривайте, извлекайте и&nbsp;делитесь<br />только тем, что нужно вам";
+$content_info_v2_text4 = "Извлечение, сжатие и просмотр<br />без ограничений";
+$content_info_v2_text5 = "Используйте стандартную команду «Поделиться»<br />для привычных действий";
 
 // Default app
-$content_defaultapp_title = "Установить Keka в качестве приложения для извлечения по умолчанию";
-$content_defaultapp_text = "Keka <a href=\"https://github.com/aonez/Keka/wiki/Default-application\">нужен помощник <i class=\"far fa-question-circle\"></i></a>, чтобы установить себя в качестве приложения по умолчанию.<br />Просто скачайте, распакуйте его, переместите на рабочий стол и затем откройте.";
+$content_defaultapp_title = "Установить Keka в&nbsp;качестве системного архиватора";
+$content_defaultapp_text = "Keka <a href=\"https://github.com/aonez/Keka/wiki/Default-application\">нужен помощник <i class=\"far fa-question-circle\"></i></a>, чтобы стать приложением по&nbsp;умолчанию.<br />Скачайте и&nbsp;извлеките его, переместите на&nbsp;Рабочий стол, а&nbsp;затем откройте.";
 
 // Privacy Policy
 $content_privacypolicy_title = "Политика конфиденциальности";
 
 // Terms of use
-$content_termsofuse_title = "Условия пользования";
+$content_termsofuse_title = "Условия использования";
 
 // Main content of the page
 $content_context_menu = "Контекстное меню";
 
 // Changelog info
 $content_changelog_title = "Взгляните на эволюцию Keka";
-$content_changelog_entry_title = "Изменения в версии";
+$content_changelog_entry_title = "Изменения в&nbsp;версии";
 $content_changelog_firstpublic = "Первый публичный релиз:";
 
 // 404
-$content_404 = "Страница не найдена";
-$content_404_start = "Вы можете перейти на <a href=\"https://www.keka.io\">главную</a>";
+$content_404 = "Страница не&nbsp;найдена";
+$content_404_start = "Вы можете перейти на&nbsp;<a href=\"https://www.keka.io\">главную</a>";
 $content_404_more = "или, может, вы ищете это?";
 
 // Maintenance
-$content_maintenance = "Will be back soon";
+$content_maintenance = "Скоро вернемся";
 
 // Bottom info
 $content_bottom_copying = "Все права защищены.";
