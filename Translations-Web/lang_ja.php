03d7c3b4d4f884862c9de260e32504ab56388c5f
SakiPapa <tadahisa.sugisaki@gmail.com>
2026-05-23 15:05:42 +0900

    Updated Japanese localization

--- Translations-Web/lang_ja.php
+++ Translations-Web/lang_ja.php
@@ -4,101 +4,101 @@
 //
 // Language: Japanese
 $content_language_locale = "ja";
-// Translated by munex-com
+// Translated by munex-com, @SakiPapa
 //
 //
 // Set $show_content_bottom_translator as true if you want to appear in the bottom of the page
 $show_content_bottom_translator = true;
-$content_bottom_translator = '翻訳: <a href="https://github.com/munex-com">munex-com</a>.';
+$content_bottom_translator = '翻訳: <a href="https://github.com/munex-com">munex-com</a>、<a href="https://github.com/SakiPapa">@SakiPapa</a>。';
 //
 //
 
 // iOS
 $ios_title = "$keka for iOS";
-$ios_content_title = "the iOS file archiver";
+$ios_content_title = "iOS用ファイルアーカイバ";
 
 // General
 $content_download = "ダウンロード";
-$content_issues = "課題";
+$content_issues = "問題";
 $content_help = "ヘルプ";
 $content_forum = "フォーラム";
 $content_changelog = "変更履歴";
-$content_title = "macOSのファイルアーカイバ";
+$content_title = "macOS用ファイルアーカイバ";
 
 // Download
 $content_platform = "$version_replace 以降が必要です";
-$content_download_text = "もっと保存、安全に共有する";
+$content_download_text = "たっぷり保存、プライバシーを保護";
 $content_download_love = "気に入りましたか?";
 $content_download_legacy = "過去のバージョン";
 $content_download_helper = "ヘルパー";
 $content_downloading = "ダウンロード中 ";
 $content_get_platform = "Get it for ";
 $content_testflight = "TestFlight needed to install this build";
-$content_downloading_if_fails = "ダウンロードが自動的に開始されない場合は ";
-$content_downloading_if_fails_click_here = "こちらをクリック";
+$content_downloading_if_fails = "ダウンロードが自動的に始まらない場合は ";
+$content_downloading_if_fails_click_here = "ここをクリック";
 
 // Like
-$content_donation_button = "寄付する";
-$content_donation_title = 'Kekaが気に入ったなら<i class="fa fa-heart" aria-hidden="true"></i>, <br />App Storeから入手するか、チップを送ってください！';
-$content_donation_mas = "App StoreからKekaを購入することで、開発を支援できます。<br />サイト上のバージョンと変わらず、App Storeを通じてのみ更新されます。";
-$content_donation_paypal = "もしApp Storeからインストールしたくなかったり、Kekaを試しただけの場合でも<br />このプロジェクトが気に入って寄付をしたい場合は、チップを送ることができます";
+$content_donation_button = "寄付";
+$content_donation_title = 'Kekaが気に入ったなら<i class="fa fa-heart" aria-hidden="true"></i>, <br />App Storeから入手するか、チップを送ってください!';
+$content_donation_mas = "App StoreからKekaを購入することで、開発を支援できます。<br />Webサイトのバージョンと同じですが、App Storeを通してのみ更新されます。";
+$content_donation_paypal = "App Storeを好まないか、Kekaを試したいだけの場合も、<br />このプロジェクトが気に入って応援したければ、チップを送ることができます";
 
 // Beta
 $content_platform_beta = "ベータ";
-$content_beta_title = "Kekaのベータ版";
-$content_beta_text = "リリースされる前に、最新機能を評価することができます。<br />もしバグを見つけたり、何か報告したいことがある場合は、";
-$content_beta_unavailable = "現在ご利用できるベータ版はありません。";
-$content_beta_up = "最新版を取得する";
+$content_beta_title = "Kekaベータ版";
+$content_beta_text = "リリース前に、最新機能を評価することができます。<br />もしバグを見つけたり、報告したいことがある場合は、";
+$content_beta_unavailable = "現在利用できるベータ版はありません。";
+$content_beta_up = "最新バージョンを入手";
 
 // Legacy
 $content_platform_legacy =  "$version_replace 向け";
 $content_legacy_title = "Kekaの過去のバージョン";
-$content_legacy_text = '時が経つにつれてあなたのMacは古くなり、Kekaの最新版を実行できなくなります。<br />でも安心してください<i class="fa fa-coffee" aria-hidden="true"></i>、古いバージョンはここにあります。';
+$content_legacy_text = '年月が経つにつれ、お使いのMacは古くなり最新版のKekaに対応できなくなりますが、<br />ご安心ください、<i class="fa fa-coffee" aria-hidden="true"></i>旧バージョンはここにあります。';
 
 // Info
-$content_info_title1 = "とてもシンプルで、とても強力";
-$content_info_title2 = "プライバシーは非常に重要です";
+$content_info_title1 = "とてもシンプル、とても強力";
+$content_info_title2 = "プライバシーはとても大切";
 $content_info_title3 = "まだ大きすぎる...";
-$content_info_text1 = "ファイルを圧縮するためにKekaを開いたり、Dockに入れてそこから使用したりする必要さえありません。<br />ファイルかフォルダをDockアイコンまたはKekaのウィンドウにドラッグアンドドロップするだけで、より簡単に使用できます。";
-$content_info_text2 = "パスワードを設定して高度に暗号化されたファイルを作成するだけで安全に共有できます。<br />7zファイルにはAES-256暗号化仕様を使用し、<br />Zip ファイルにはZip 2.0レガシー暗号化仕様を使用します。";
-$content_info_text3 = "ファイルが非常に大きく、メールやサーバーに収まらない場合は、ファイルを分割してください。<br />心配する必要はありません。元のファイルを生成するために拡張されます(^^)";
+$content_info_text1 = "ファイルを圧縮するためにKekaを開いたり、Dockに入れて使用したりする必要などありません。<br />ファイルやフォルダを、DockのアイコンかKekaのウインドウにドロップするだけでスリムにできます。";
+$content_info_text2 = "パスワードを設定して高度に暗号化すれば、安全にファイルを共有できます。<br />7zファイルにはAES-256暗号化を、<br />Zip ファイルにはZip 2.0レガシー暗号化を使用します。";
+$content_info_text3 = "巨大なファイルで、メール添付やサーバに収まらない場合は、ファイルを分割してください。<br />安心してください。ファイルが元どおり生成されます(^^)";
 $content_info_compression = "Kekaは次の形式のファイルを作成できます:";
-$content_info_extraction = "そして、これらのすべての形式が復元できます:";
+$content_info_extraction = "これらすべての形式の展開が可能です:";
 
 // Info v2 (iOS and future macOS)
-$content_info_v2_title1 = "Store more";
-$content_info_v2_title2 = "Share with privacy";
-$content_info_v2_title3 = "A perfect Browsing experience";
-$content_info_v2_title4 = "Multitasking at its finest";
-$content_info_v2_title5 = "Always at hand";
-$content_info_v2_text1 = "With multiple Compression Formats<br />to choose the Perfect One";
-$content_info_v2_text2 = "Protect your shared files with a Password<br />and Encrypt them with AES-256";
-$content_info_v2_text3 = "Preview, Extract and Share<br />only what You Need";
-$content_info_v2_text4 = "Extract, Compress and Browse<br />without limits";
-$content_info_v2_text5 = "Extract, Compress and Browse<br />from anywhere using the Share Actions";
+$content_info_v2_title1 = "たっぷり保存";
+$content_info_v2_title2 = "プライバシーを保って共有";
+$content_info_v2_title3 = "完璧なブラウズ体験";
+$content_info_v2_title4 = "マルチタスクの神髄";
+$content_info_v2_title5 = "いつでも手元に";
+$content_info_v2_text1 = "複数の圧縮形式に対応<br />最適な形式を選べる";
+$content_info_v2_text2 = "パスワードとAES-256暗号化により<br />共有ファイルを保護";
+$content_info_v2_text3 = "思いどおりに<br />プレビュー、展開、共有";
+$content_info_v2_text4 = "無制限に<br />展開、圧縮、プレビュー";
+$content_info_v2_text5 = "共有アクションを使えば<br />どこでも展開、圧縮、ブラウズ";
 
 // Default app
-$content_defaultapp_title = "Kekaをデフォルトのアーカイバとして設定する";
-$content_defaultapp_text = 'Kekaをデフォルトのアプリケーションとして設定するためには、<a href="https://github.com/aonez/Keka/wiki/Default-application">ヘルパーが必要です。<i class="far fa-question-circle"></i></a><br />ダウンロードして解凍し、デスクトップに移動させてから開くだけです。';
-
-// Privacy Policy
-$content_privacypolicy_title = 'プライバシーポリシー';
+$content_defaultapp_title = "Kekaをデフォルトのアーカイバにする";
+$content_defaultapp_text = 'Kekaをデフォルトの展開アプリに設定するには、<a href="https://github.com/aonez/Keka/wiki/Default-application">ヘルパー<i class="far fa-question-circle"></i></a>が必要です。<br />ダウンロードして展開し、デスクトップに移動させて開いてください。';
 
 // Terms of use
 $content_termsofuse_title = '利用規約';
 
+// Privacy Policy
+$content_privacypolicy_title = 'プライバシーポリシー';
+
 // Main content of the page
-$content_context_menu = "コンテキストメニュー";
+$content_context_menu = "コンテクストメニュー";
 
 // Changelog info
 $content_changelog_title = "Kekaの進化をご覧ください";
-$content_changelog_entry_title = "変更点 version";
+$content_changelog_entry_title = "各バージョンの変更内容";
 $content_changelog_firstpublic = "最初の公開リリース:";
 
 // 404
 $content_404 = "ページが見つかりません";
-$content_404_start = "<a href=\"https://www.keka.io\">スタートページ</a>へ行くことが可能です";
-$content_404_more = "それともお探しのものはこちらでしょうか？";
+$content_404_start = "<a href=\"https://www.keka.io\">スタートページ</a>";
+$content_404_more = "それともこちらをお探しですか?";
 
 // Maintenance
 $content_maintenance = "Will be back soon";
